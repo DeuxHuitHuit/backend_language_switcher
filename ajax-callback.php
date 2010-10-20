ed0d782cf672e0e3fdcaadbd58d7093402079334
John J. Camilleri <john@johnjcamilleri.com>
2010-10-20 09:34:10 +0200

    Removed ajax-callback.php

--- ajax-callback.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Script for changing a user's language (directly in the database)
- */
-
-if (!$_REQUEST['action[save]']) die();
-$username = $_REQUEST['fields[username]'];
-$language = $_REQUEST['fields[language]'];
-?>
